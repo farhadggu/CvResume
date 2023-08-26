a7ecbc3f79cf93d66b913bb11a1339a33398bb61
farhadggu <farhadggu@gmail.com>
2023-08-26 17:12:55 +0330

    Updates

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+(function ($) {
+  $(document).ready(function () {
+    $(document).on("click", ".header-area .show-menu", function () {
+      $(this).toggleClass("active");
+      $(".header-area .navbar").toggleClass("active");
+    });
+    // $(document).on('click', '.header-area .navbar .close-menu', function() {
+    //     $(".header-area .navbar").removeClass('active');
+    // });
+
+  });
+})(jQuery);
